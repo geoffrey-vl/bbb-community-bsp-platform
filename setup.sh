2e4ecdbf66b8a1b0d8220b2bad33708edc609f71
Geoffrey Van Landeghem <geoffreyvanlandeghem@alphatronics.be>
2018-12-03 09:23:26 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+REPODIR=${PWD}
+
+echo $REPODIR
+
+source sources/poky/oe-init-build-env build
+
+cat <<EOF > conf/bblayers.conf
+# POKY_BBLAYERS_CONF_VERSION is increased each time build/conf/bblayers.conf
+# changes incompatibly
+POKY_BBLAYERS_CONF_VERSION = "2"
+
+BBPATH = "\${TOPDIR}"
+BBFILES ?= ""
+
+BBLAYERS ?= " \\
+    ${REPODIR}/sources/poky/meta \\
+    ${REPODIR}/sources/poky/meta-poky \\
+    ${REPODIR}/sources/meta-openembedded/meta-oe \\
+    ${REPODIR}/sources/meta-openembedded/meta-multimedia \\
+    ${REPODIR}/sources/meta-openembedded/meta-networking \\
+    ${REPODIR}/sources/meta-openembedded/meta-perl \\
+    ${REPODIR}/sources/meta-openembedded/meta-python \\
+    ${REPODIR}/sources/meta-qt5 \\
+    ${REPODIR}/sources/meta-bbb \\
+    ${REPODIR}/sources/meta-security \\
+"
+EOF
+
+cp ../sources/meta-bbb/conf/local.conf.sample conf/local.conf
